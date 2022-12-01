88b75d099c689f0c3923eed1487e7315511537f9
111360210 <t111360210@ntut.org.tw>
2022-12-01 18:50:13 +0800

    first commit

---
+++ 6.36/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void revstr(char *str1) 
+{
+	int i, len, temp;
+	len = strlen(str1);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = str1[i]; 
+		str1[i] = str1[len - i - 1]; 
+		str1[len - i - 1] = temp; 
+	} 
+} 
+
+int main() 
+{ 
+	char str[50] = {"HELLO"};
+	
+	printf ("Before reversing the string\t: %s \n", str); 
+
+	revstr(str); 
+	printf ("After reversing the string\t: %s", str); 
+}
